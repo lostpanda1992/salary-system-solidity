e5da8dfd3443399f6e9c0250b8a8905e0dcdc231
yu chen <1097977926@qq.com>
2022-06-26 11:46:11 +0800

    添加部分开源功能代码

---
+++ salary/Ownable.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.18;
+
+
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
